e365ca1d150614597111a2303524efeec91d8c8a
mapper895 <mapper.895.mp@gmail.com>
2021-04-11 18:19:08 -0500

    Add Rotation

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
+import React, { useRef } from "react";
 import "./App.scss";
-import { Canvas } from "react-three-fiber";
+import { Canvas, useFrame } from "react-three-fiber";
 
 function App() {
   return (
     <>
       <Canvas>
-        <mesh>
-          <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
-          <meshStandardMaterial attach="material" />
-        </mesh>
+        <Box />
       </Canvas>
     </>
   );
 }
+
+const Box = () => {
+  const mesh = useRef(null);
+  useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
+
+  return (
+    <mesh ref={mesh}>
+      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+      <meshStandardMaterial attach="material" />
+    </mesh>
+  );
+};
 
 export default App;
