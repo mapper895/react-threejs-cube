b6f6008b4f7ef55eaf72ad6ddd4cc2692d79476f
mapper895 <mapper.895.mp@gmail.com>
2021-04-11 18:35:42 -0500

    Add Lights

--- src/App.js
+++ src/App.js
@@ -5,21 +5,37 @@ import { Canvas, useFrame } from "react-three-fiber";
 function App() {
   return (
     <>
-      <Canvas>
-        <Box />
+      <Canvas colorManagement camera={{ position: [-5, 2, 10], fov: 60 }}>
+        <ambientLight intensity={0.3} />
+        <directionalLight
+          position={[0, 10, 0]}
+          intensity={1.5}
+          shadow-mapSize-width={1024}
+          shadow-mapSize-height={1024}
+          shadow-camera-far={50}
+          shadow-camera-left={-10}
+          shadow-camera-right={10}
+          shadow-camera-top={10}
+          shadow-camera-bottom={-10}
+        />
+        <pointLight position={[-10, 0, -20]} intensity={0.5} />
+        <pointLight position={[0, -10, 0]} intensity={1.5} />
+        <SpinningMesh position={[0, 1, 0]} args={[3, 2, 1]} color="lightblue" />
+        <SpinningMesh position={[-2, 1, -5]} color="pink" />
+        <SpinningMesh position={[5, 1, -2]} color="pink" />
       </Canvas>
     </>
   );
 }
 
-const Box = () => {
+const SpinningMesh = ({ position, args, color }) => {
   const mesh = useRef(null);
   useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
 
   return (
-    <mesh ref={mesh}>
-      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
-      <meshStandardMaterial attach="material" />
+    <mesh position={position} ref={mesh}>
+      <boxBufferGeometry attach="geometry" args={args} />
+      <meshStandardMaterial attach="material" color={color} />
     </mesh>
   );
 };
